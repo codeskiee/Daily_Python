5b70fa1315b93229a03d93f9aba349ff706ea057
codeskiee <code.skiee99@gmail.com>
2024-08-03 20:11:13 +0700

    Add files via upload

---
+++ Indentation.py
@@ -0,0 +1,10 @@
+def salampagi (nama) :
+    print ("Haloo..")
+    print ("Selamat Pagi",nama)
+    print ("Apa kabarmu hari ini?")
+
+salampagi("Joni")
+
+# CATATAN
+#    Identation merupakan penataan baris-baris kode program yang mengatur bagian mana yang menjorok 
+#   kedalam dan yang tidak, untuk menandai blok-blok program yang kita buat.S
