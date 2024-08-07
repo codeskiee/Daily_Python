bb40859a7c8ffd65d01f46bcf0faa83f98080adf
codeskiee <code.skiee99@gmail.com>
2024-08-07 19:40:00 +0700

    Add files via upload

---
+++ Coloring_Text.py
@@ -0,0 +1,14 @@
+#Perwarnaan teks warna hijau dan background warna hitam
+print ("\033[1;32;40m Tulisan ini berwarna loh...\n")
+
+#Perwarnaan teks warna merah dan background warna putih
+print ("\033[2;31;47m Tulisan ini berwarna loh...\n")
+
+#CATATAN
+#   Perintah yang digunakan     'print'
+#   Escape kode \033 digunakan untuk menampilkan ANSI Code
+#   Perintah style teks yang digunakan angka '0 s/d 5'
+#   Perintah warna teks yang digunakan angka '30 s/d 37'
+#   Perintah warna latar belakang digunakan '40 s/d 47'
+
+#Untuk source berada di halaman 41
