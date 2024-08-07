1d8a43eec8cfe5421c0ba374473530f4f486afad
codeskiee <code.skiee99@gmail.com>
2024-08-07 19:39:37 +0700

    Add files via upload

---
+++ Function_End.py
@@ -0,0 +1,6 @@
+# Argumen END adalah perintah untuk membuat 2 teks atau lebih menjadi satu baris
+
+print ('Selamat Datang...')
+nama = input ("Silahkan memasukkan nama :")
+print ("Salam kenal ",end='')
+print (nama)
